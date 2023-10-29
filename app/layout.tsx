375922b2b9ec6cdda1b1169efcd31af63ab282d6
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-29 23:38:29 +1200

    first commit SideBar under develop

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import './globals.css'
+import SideBar from '@/components/SideBar'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'That Music Box',
+  description: 'Lets buy and sell some melodies!',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <SideBar>
+          {children}
+        </SideBar>
+        
+        
+      </body>
+    </html>
+  )
+}
