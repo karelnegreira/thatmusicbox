5a37c9b03b4ec496e4d2fb51e0cf62d5523dd8f4
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-10-30 22:44:04 +1200

    creating sidebaritem component

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -6,6 +6,7 @@ import { useMemo } from "react";
 import {HiHome} from 'react-icons/hi';
 import {BiSearch} from 'react-icons/bi';
 import Box from "./Box";
+import SidebarItem from "./SidebarItem";
 
 
 interface SideBarProps {
@@ -43,7 +44,19 @@ const SideBar: React.FC<SideBarProps> = ( {children }) => {
         "
         >
             <Box>
-                Sidebar navigation bar
+                <div 
+                className="
+                    flex
+                    flex-col
+                    gap-y-4
+                    px-5
+                    py-4
+                "
+                >
+                    {routes.map((item) => (
+                        <SidebarItem key={item.label} {...item} />
+                    ))}
+                </div>
             </Box>
             <Box className="overflow-auto h-full">
                 Song library
