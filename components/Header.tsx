d948a4f4d5efbc3cdcee39ad3011858834ab7e89
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-11-01 22:18:19 +1200

    Sidebar header mobile version implemented

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,7 @@
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
+import { HiHome } from "react-icons/hi";
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -21,13 +22,19 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
     <div className={twMerge(`h-fit bg-gradient-to-b from-slate-800 p-6`, className)}>
         <div className="w-full mb-4 flex items-center justify-between">
             <div className="hidden md:flex gap-x-2 items-center">
-                <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                <button onClick={() => router.back()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
                     <RxCaretLeft  className="text-white" size={35} />
                 </button>
 
-                <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                <button onClick={() => router.forward()} className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
                     <RxCaretRight  className="text-white" size={35} />
                 </button>
+            </div>
+            <div className="flex md:hidden gap-x-2 items-center">
+                <button className="rounded-full p-2 bg-white flex items-center justify-center hover:opacity-75 transition">
+                    <HiHome size={35} className="text-black"/>
+                </button>
+
             </div>
         </div>
     </div>
