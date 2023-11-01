64d23927694a636d9e15658494ee608f28a41a8b
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-11-01 22:03:27 +1200

    adding arrow left button

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { twMerge } from "tailwind-merge";
+import { RxCaretLeft } from 'react-icons/rx';
 
 interface HeaderProps {
     children: React.ReactNode;
@@ -17,8 +18,14 @@ const Header: React.FC<HeaderProps> = ({children, className}) => {
     }
 
   return (
-    <div className={twMerge(`h-fit bg-gradient-to-b from-slate-800 p-6`)}>
-
+    <div className={twMerge(`h-fit bg-gradient-to-b from-slate-800 p-6`, className)}>
+        <div className="w-full mb-4 flex items-center justify-between">
+            <div className="hidden md:flex gap-x-2 items-center">
+                <button className="rounded-full bg-black flex items-center justify-center hover:opacity-75 transition">
+                    <RxCaretLeft  className="text-white" size={35} />
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
