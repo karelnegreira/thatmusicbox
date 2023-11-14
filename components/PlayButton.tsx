e8c297212adf9b2575b53c68f2a04c7be69d0278
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-11-15 02:28:45 +1200

    PlayButton component was created

---
+++ components/PlayButton.tsx
@@ -0,0 +1,11 @@
+
+
+const PlayButton = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default PlayButton
--- components/SongItem.tsx
+++ components/SongItem.tsx
@@ -3,6 +3,7 @@
 import useImageLoader from "@/hooks/useImageLoader";
 import { Song } from "@/types";
 import Image from "next/image";
+import PlayButton from "./PlayButton";
 
 interface SongItemProps {
     data: Song;
@@ -20,6 +21,18 @@ const SongItem: React.FC<SongItemProps> = ({data, onClick}) => {
                      hover:bg-neutral-400/10 transition p-3" >
         <div className="relative aspect-square w-full h-full rounded-md overflow-hidden">
             <Image className="object-cover" src={imagePath || '/images/liked.png' } fill alt="Image" />
+        </div>
+        <div className="flex flex-col items-start w-full pt-4 gap-y-1">
+            <p className="font-semibold truncate w-full">
+                {data.title}
+            </p>
+            <p className="text-neutral-400 text-sm pb-4 w-full truncate">
+              By {data.author}  
+            </p>
+        </div>
+        
+        <div className="absolute bottom-24 right-5">
+            <PlayButton />
         </div>
     </div>
   )
