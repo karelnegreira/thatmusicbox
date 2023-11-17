db71c5463de50fe2b94b52b8e2319666f7f2023e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-11-18 07:20:06 +1200

    Library songs props worked

--- components/Library.tsx
+++ components/Library.tsx
@@ -6,8 +6,13 @@ import {AiOutlinePlus} from 'react-icons/ai';
 import useAuthModal from '@/hooks/useAuthModal';
 import { useUser } from '@/hooks/useUser';
 import useUploadModal from '@/hooks/useUploadModal';
+import { Song } from '@/types';
 
-const Library = () => {
+interface LibraryProps {
+    songs: Song[];
+}
+
+const Library: React.FC<LibraryProps> = ({songs}) => {
     const authModal = useAuthModal();
     const uploadModal = useUploadModal();
     const { user } = useUser();
@@ -32,7 +37,9 @@ const Library = () => {
             <AiOutlinePlus onClick={onClick} size={20} className="text-neutral-400 cursor-pointer hover:text-white transition" />
         </div>
         <div className="flex flex-col gap-y-2 mt-4 px-3">
-            Your list of songs
+            {songs.map((item) => (
+                <div>{item.title}</div>
+            ))}
         </div>
     </div>
   )
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -8,13 +8,15 @@ import {BiSearch} from 'react-icons/bi';
 import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
+import { Song } from "@/types";
 
 
 interface SideBarProps {
     children: React.ReactNode;
+    songs: Song[];
 }
 
-const SideBar: React.FC<SideBarProps> = ( {children }) => {
+const SideBar: React.FC<SideBarProps> = ( {children, songs }) => {
     const pathname = usePathname();
     const routes = useMemo(() => [
         {
@@ -60,7 +62,7 @@ const SideBar: React.FC<SideBarProps> = ( {children }) => {
                 </div>
             </Box>
             <Box className="overflow-auto h-full">
-                <Library />
+                <Library songs={songs} />
             </Box>
         </div>  
         <main className="h-full flex-1 overflow-y-auto py-2">
