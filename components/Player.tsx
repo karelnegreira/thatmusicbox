511481beb1942b869a408c9b6dee9db607269794
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-11-24 16:23:32 +1200

    useLoadSongUrl created and implemented

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,6 +1,15 @@
 "use client";
 
+import useGetSongsById from "@/hooks/useGetSongsById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
+import usePlayer from "@/hooks/usePlayer";
+
 const Player = () => {
+
+    const player = usePlayer();
+    const { song } = useGetSongsById(player.activeId);
+    const songUrl = useLoadSongUrl(song);
+
   return (
     <div>Player</div>
   )
