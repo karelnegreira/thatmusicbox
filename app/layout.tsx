083d4cf6b63dede8a80d7b7663e09b83a4a4fa3b
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-11-24 15:47:51 +1200

    Player component declared only

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import UserProvider from '@/providers/UserProvider'
 import ModalProvider from '@/providers/ModalProvider'
 import ToasterProvider from '@/providers/ToasterProvider'
 import getSongsByUserId from '@/functions/getSongsByUserId'
+import Player from '@/components/Player'
 
 
 const font = Figtree({ subsets: ['latin'] })
@@ -36,6 +37,7 @@ export default async function RootLayout({
               <SideBar songs={userSongs}>
                   {children}
               </SideBar>
+              <Player />
             </UserProvider>
         </SupabaseProvider>
         
