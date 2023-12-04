d01412518a2870966028494253ef8f926a4ae79a
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-12-05 11:52:05 +1200

    Slider value equals 1 fix the bug attempt 1

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -7,7 +7,7 @@ interface SliderProps {
     onChange?: (value: number) => void;
 }
 
-const Slider: React.FC<SliderProps> = ({value, onChange}) => {
+const Slider: React.FC<SliderProps> = ({value = 1, onChange}) => {
 
     const handleChange = (newValue: number[]) => {
         onChange?.(newValue[0]);
