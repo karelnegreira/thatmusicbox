8eea7c8a3ea6df34e9ff196203a906b1ca687075
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-12-04 16:33:36 +1200

    Playing songs content up and running everywhere in the app

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -9,6 +9,8 @@ import Box from "./Box";
 import SidebarItem from "./SidebarItem";
 import Library from "./Library";
 import { Song } from "@/types";
+import usePlayer from "@/hooks/usePlayer";
+import { twMerge } from "tailwind-merge";
 
 
 interface SideBarProps {
@@ -18,6 +20,8 @@ interface SideBarProps {
 
 const SideBar: React.FC<SideBarProps> = ( {children, songs }) => {
     const pathname = usePathname();
+    const player = usePlayer();
+
     const routes = useMemo(() => [
         {
             icon: HiHome, 
@@ -33,7 +37,12 @@ const SideBar: React.FC<SideBarProps> = ( {children, songs }) => {
         }
     ], [pathname]);
   return (
-    <div className="flex h-full">
+    <div className={twMerge(`
+        flex
+        h-full
+    `, 
+    player.activeId && "h-[calc(100%-80px)]"
+    )}>
         <div 
         className="
             hidden
