83b0d37fddfc147286553bfc503a687cef07b54e
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-12-10 23:00:58 +1200

    copyBillingDetailsToCustomer implemented in supbaseAdmin

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -102,10 +102,18 @@ const copyBillingDetailsToCustomer = async (
     const {name, phone, address } = payment_method.billing_details;
     if (!name || !phone || !address) return;
     
-    //ts-ignore
+    //@ts-ignore
     await stripe.customers.update(customer, {name, phone, address});
 
+    const {error} = await supabaseAdmin
+        .from('users')
+        .update({
+            billing_address: {...address},
+            payment_method: {...payment_method[payment_method.type]}
+        })
+        .eq('id', uuid);
     
-}
+    if (error) throw error;
+};
 
 
