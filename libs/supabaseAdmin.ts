6928c158fdcdc6214bbbaab7c49881e262008f37
Karel Negreira Fuentes <karelnegreira@gmail.com>
2023-12-11 17:12:55 +1200

    route and supabaseAdmin are created and completed

--- libs/supabaseAdmin.ts
+++ libs/supabaseAdmin.ts
@@ -119,7 +119,7 @@ const copyBillingDetailsToCustomer = async (
 const manageSubscriptionStatusChange = async (
     subscriptionId: string, 
     customerId: string, 
-    createAction: false 
+    createAction = false 
 ) => {
     const { data: customerData, error: noCustomerError } = await supabaseAdmin.from('customers').select('id').eq('stripe_customer_id', customerId).single();
 
